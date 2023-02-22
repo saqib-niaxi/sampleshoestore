19a9cb3aa0a9502e406ec37523a6b6ee8cdb48cb
Muhammad Saqib Khan <91668456+saqib-niaxi@users.noreply.github.com>
2023-02-22 08:01:23 +0500

    Updates

---
+++ precache-manifest.f291566835220a9187d918349634f819.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a8da93f80ff1071fcc4e2e99a29a603",
+    "url": "/sampleshoestore/index.html"
+  },
+  {
+    "revision": "2a4febf69d772995a68f",
+    "url": "/sampleshoestore/static/css/main.bedf8759.chunk.css"
+  },
+  {
+    "revision": "f116113a762b0032b98f",
+    "url": "/sampleshoestore/static/js/2.e6f88944.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/sampleshoestore/static/js/2.e6f88944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a4febf69d772995a68f",
+    "url": "/sampleshoestore/static/js/main.97a85dc1.chunk.js"
+  },
+  {
+    "revision": "03eb573673fa644bd35b",
+    "url": "/sampleshoestore/static/js/runtime-main.f0e3399a.js"
+  }
+]);
